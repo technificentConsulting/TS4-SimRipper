0b55181ed0f1d699d8f8524ce006bc8597ee3681
thepancake1 <theojabalde@gmail.com>
2022-02-12 02:17:18 +0800

    Fix waistfiller

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace TS4SimRipper.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -89,12 +89,14 @@ namespace TS4SimRipper.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        internal static byte[] WaistFiller
-        {
-            get
-            {
-                object @object = Resources.ResourceManager.GetObject("WaistFiller", Resources.resourceCulture);
-                return (byte[])@object;
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] WaistFiller {
+            get {
+                object obj = ResourceManager.GetObject("WaistFiller", resourceCulture);
+                return ((byte[])(obj));
             }
         }
     }
